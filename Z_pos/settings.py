3dbd4a2b0df65f9d2073fac0f67364290972b8f2
M.Abdullah.R <myworkatabwa@gmail.com>
2025-12-16 17:45:29 +0000

    Update settings.py

--- Z_pos/settings.py
+++ Z_pos/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-default-key')
 DEBUG = os.environ.get('DJANGO_DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
+ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', '127.0.0.1,localhost','epos-production-c366.up.railway.app').split(',')
 CSRF_TRUSTED_ORIGINS = os.environ.get('DJANGO_CSRF_TRUSTED_ORIGINS', 'https://*.railway.app').split(',')
 
 # =====================================================
