4d6cec3985b87a6190ba51c15dd497326bdb4821
M.Abdullah.R <myworkatabwa@gmail.com>
2025-12-16 18:05:57 +0000

    Update settings.py

--- Z_pos/settings.py
+++ Z_pos/settings.py
@@ -86,9 +86,9 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "railway",
         "USER": "postgres",
-        "PASSWORD": "postgres",
-        "HOST": "localhost",
-        "PORT": "5432",
+        "PASSWORD": "uqiebknQVCAaFZPfsnlbWJCTSYMrwwMX",
+        "HOST": "shuttle.proxy.rlwy.net",
+        "PORT": "11255",
     }
 }
 
