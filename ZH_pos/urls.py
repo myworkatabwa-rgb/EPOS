2f705bb32551977c35811e328f2a8fa408aa7bbf
M.Abdullah.R <myworkatabwa@gmail.com>
2025-12-17 18:36:23 +0000

    Update urls.py

--- ZH_pos/urls.py
+++ ZH_pos/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.dashboard, name='dashboard'),
-    path("pos/checkout/", views.pos_checkout, name="pos_checkout"),
-    path("pos/checkout/", pos_checkout, name="pos_checkout"),
+    path("pos/checkout/", views.pos_checkout, name="pos_checkout"),  # use views.pos_checkout
+    path("pos/", views.pos_view, name="pos_view"),  # optional, for POS page
 ]
