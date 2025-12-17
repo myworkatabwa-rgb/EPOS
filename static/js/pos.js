123d508c5bb5f6b87d5b8625b9d479d2b5c95403
M.Abdullah.R <myworkatabwa@gmail.com>
2025-12-17 18:16:55 +0000

    Update pos.js

--- static/js/pos.js
+++ static/js/pos.js
@@ -77,3 +77,72 @@ function getCookie(name) {
     }
     return cookieValue;
 }
+let cart = {};
+
+function addToCart(id, name, price) {
+    if (!cart[id]) {
+        cart[id] = { name, price, qty: 1 };
+    } else {
+        cart[id].qty += 1;
+    }
+    renderCart();
+}
+
+function renderCart() {
+    const cartDiv = document.getElementById("cart-items");
+    const totalSpan = document.getElementById("total");
+
+    cartDiv.innerHTML = "";
+    let total = 0;
+
+    Object.values(cart).forEach(item => {
+        total += item.price * item.qty;
+
+        cartDiv.innerHTML += `
+            <div class="cart-row">
+                <span>${item.name} x ${item.qty}</span>
+                <span>PKR ${item.price * item.qty}</span>
+            </div>
+        `;
+    });
+
+    totalSpan.innerText = total;
+}
+
+function checkout() {
+    if (Object.keys(cart).length === 0) {
+        alert("Cart is empty");
+        return;
+    }
+
+    fetch("/pos/checkout/", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "X-CSRFToken": getCookie("csrftoken")
+        },
+        body: JSON.stringify(cart)
+    })
+    .then(res => res.json())
+    .then(data => {
+        if (data.success) {
+            alert("Order placed successfully");
+            cart = {};
+            renderCart();
+        } else {
+            alert(data.error);
+        }
+    });
+}
+
+// CSRF Helper
+function getCookie(name) {
+    let cookieValue = null;
+    document.cookie.split(";").forEach(cookie => {
+        cookie = cookie.trim();
+        if (cookie.startsWith(name + "=")) {
+            cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+        }
+    });
+    return cookieValue;
+}
