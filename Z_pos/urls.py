47741d4a300f1e168093295d9433e97981b068e3
M.Abdullah.R <myworkatabwa@gmail.com>
2025-12-18 17:35:17 +0000

    Update urls.py

--- Z_pos/urls.py
+++ Z_pos/urls.py
@@ -2,14 +2,37 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from ZH_pos.woocommerce_webhook import woocommerce_webhook
+from django.contrib.auth import views as auth_views
 
+from ZH_pos.woocommerce_webhook import woocommerce_webhook
+from ZH_pos.views import pos_view
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('ZH_pos.urls')),
+    path("admin/", admin.site.urls),
+
+    # Auth
+    path(
+        "login/",
+        auth_views.LoginView.as_view(
+            template_name="login.html",
+            redirect_authenticated_user=True
+        ),
+        name="login",
+    ),
+    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
+
+    # POS
+    path("pos/", pos_view, name="pos"),
+
+    # WooCommerce webhook
     path("webhook/woocommerce/", woocommerce_webhook),
+
+    # App URLs (dashboard, etc.)
+    path("", include("ZH_pos.urls")),
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )
