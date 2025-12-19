5a2916d4e1fd4f56ce1b906c994beec925be931b
M.Abdullah.R <myworkatabwa@gmail.com>
2025-12-18 23:15:32 -0800

    Create transaction.py

---
+++ ZH_pos/views/transaction.py
@@ -0,0 +1,11 @@
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
+
+
+@login_required
+def sale_returns(request):
+    return render(request, "transactions/sale_returns.html")
+
+@login_required 
+def sale_return_history(request):
+    return render (request, "transactions/sale_return_history.html")
