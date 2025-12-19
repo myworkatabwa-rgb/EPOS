db486fc6988c7678c71de864e5824b7fe674b3e8
M.Abdullah.R <myworkatabwa@gmail.com>
2025-12-18 21:47:25 -0800

    Update urls.py

--- ZH_pos/urls.py
+++ ZH_pos/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from . import views
+from ZH_pos import views
 
 urlpatterns = [
-    path('', views.dashboard, name='dashboard'),
-    path("pos/checkout/", views.pos_checkout, name="pos_checkout"),  # use views.pos_checkout
-    path("pos/", views.pos_view, name="pos_view"),  # optional, for POS page
+    path("", views.dashboard, name="dashboard"),
+    path("pos/", views.pos_view, name="pos"),
+    path("pos/checkout/", views.pos_checkout, name="pos_checkout"),
+    path("returns/<str:order_id>/", views.process_return, name="process_return"),
 ]
