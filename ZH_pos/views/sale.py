5aee6019d6470ca3e13c652997afce25064082e4
M.Abdullah.R <myworkatabwa@gmail.com>
2025-12-20 17:54:56 +0000

    Update sale.py

--- ZH_pos/views/sale.py
+++ ZH_pos/views/sale.py
@@ -1,13 +1,24 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from zh.pos import order
 
 @login_required
 def sell(request):
     return render(request, "sales/sell.html")
 
-@login_required
+@login_required(login_url="/login/")
 def sale_history(request):
-    return render (request, "sales/sale_history.html")
+    sales = (
+        Order.objects
+        .select_related("customer")
+        .order_by("-created_at")
+    )
+
+    return render(
+        request,
+        "sales/sale_history.html",
+        {"sales": sales}
+    )
 
 @login_required 
 def ecommerce_orders(request):
