19e36b74580d9fe0ddcdd64876fddad68b156ff4
M.Abdullah.R <myworkatabwa@gmail.com>
2025-12-20 18:03:19 +0000

    Update sale.py

--- ZH_pos/views/sale.py
+++ ZH_pos/views/sale.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
-from ZH_pos import order
+from ZH.pos import order
 
 @login_required
 def sell(request):
