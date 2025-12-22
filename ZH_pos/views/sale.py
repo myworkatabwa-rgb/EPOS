1f458064d8ee95bfaa30d6b4773726e3ab856a01
M.Abdullah.R <myworkatabwa@gmail.com>
2025-12-22 03:52:56 -0800

    Update sale.py

--- ZH_pos/views/sale.py
+++ ZH_pos/views/sale.py
@@ -2,7 +2,7 @@ from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import permission_required
-from ZH_Pos.models import Product
+from ZH_pos.models import Product
 
 
 from ZH_pos.models import Order
