9f87cb4d892bb187881574b56c74d06fd3b1fd4e
zhepos8-lang <zhepos8@gmail.com>
2026-01-31 02:19:19 -0800

    Update returns.py

--- ZH_pos/views/returns.py
+++ ZH_pos/views/returns.py
@@ -3,7 +3,7 @@ from django.shortcuts import render, get_object_or_404
 from django.http import JsonResponse
 import json
 
-from .models import Order, OrderItem, Return
+from ZH_pos.models import Order, OrderItem, Return
 
 
 @login_required
