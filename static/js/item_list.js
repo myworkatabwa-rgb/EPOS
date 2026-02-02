51d1a1e8a0419381d7606b6e74ccb4954793c024
zhepos8-lang <zhepos8@gmail.com>
2026-02-01 22:20:02 -0800

    Update item_list.js

--- static/js/item_list.js
+++ static/js/item_list.js
@@ -17,3 +17,47 @@ $(document).ready(function () {
         table.search(this.value).draw();
     });
 });
+$(document).ready(function () {
+
+    const table = $('#itemTable').DataTable({
+        pageLength: 10,
+        dom: 'Bfrtip',
+        buttons: ['excel', 'csv', 'pdf', 'colvis']
+    });
+
+    table.buttons().container().appendTo('#tableButtons');
+
+    $('#itemSearch').on('keyup', function () {
+        table.search(this.value).draw();
+    });
+
+    // IMPORT FORM
+    $('#importForm').on('submit', function (e) {
+        e.preventDefault();
+
+        let formData = new FormData(this);
+
+        $('#importStatus').html('<span class="text-info">Importing...</span>');
+
+        $.ajax({
+            url: "/items/import/",
+            method: "POST",
+            data: formData,
+            processData: false,
+            contentType: false,
+            success: function () {
+                $('#importStatus').html('<span class="text-success">Import successful</span>');
+                $('#importModal').modal('hide');
+
+                // Reload page to refresh items
+                location.reload();
+            },
+            error: function (xhr) {
+                $('#importStatus').html(
+                    '<span class="text-danger">' + xhr.responseText + '</span>'
+                );
+            }
+        });
+    });
+
+});
