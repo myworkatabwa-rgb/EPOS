826dfde2f1cd46633d008fcc5d81798ce35c9ce9
zhepos8-lang <zhepos8@gmail.com>
2026-02-04 03:16:24 -0800

    Update items.py

--- ZH_pos/views/items.py
+++ ZH_pos/views/items.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_POST
 from django.shortcuts import render
 from ZH_pos.models import Product
 import csv
