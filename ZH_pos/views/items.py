562530e5a9f846f2ffdaa6e4b0f152c78852bfb4
zhepos8-lang <zhepos8@gmail.com>
2026-02-13 18:39:49 +0000

    Update items.py

--- ZH_pos/views/items.py
+++ ZH_pos/views/items.py
@@ -361,7 +361,7 @@ def barcode_preview(request):
         items.append({
             "name": product.name,
             "sku": product.sku,
-            "price": product.price,
+            "price": product.sale_price,
             "barcode": barcode_image
         })
 
