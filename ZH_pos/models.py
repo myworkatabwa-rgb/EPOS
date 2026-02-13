87e1ea895a1f7d4955ddcd8dbe222dc988a53e9e
zhepos8-lang <zhepos8@gmail.com>
2026-02-12 21:55:14 -0800

    Update models.py

--- ZH_pos/models.py
+++ ZH_pos/models.py
@@ -99,7 +99,16 @@ class Supplier(models.Model):
 
     def __str__(self):
         return self.supplier_name
+        
+class Brand(models.Model):
 
+    brand_code = models.CharField(max_length=50, unique=True)
+    brand_name = models.CharField(max_length=255)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.brand_name
 # =========================
 # ORDER (SALE)
 # =========================
