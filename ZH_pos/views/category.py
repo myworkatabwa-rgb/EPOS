97b505b08eaee8426c911ce66d4dd2a274665599
zhepos8-lang <zhepos8@gmail.com>
2026-02-16 22:10:04 -0800

    Create category.py

---
+++ ZH_pos/views/category.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from ...models import Category
+from ...woocommerce_api import sync_woo_categories
+
+
+@login_required
+def list_category(request):
+
+    # 🔥 Call sync from woocommerce_api.py
+    sync_woo_categories()
+
+    categories = Category.objects.all()
+
+    return render(request, "category/list_category.html", {
+        "categories": categories
+    })
+@login_required
+def add_category(request):
+
+    if request.method == "POST":
+        name = request.POST.get("name")
+
+        Category.objects.create(name=name)
+
+        return redirect("list_category")
+
+    return render(request, "category/add_category.html")
