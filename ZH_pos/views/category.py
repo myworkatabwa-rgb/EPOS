22b5a015dca83efce913969408a26c6435374759
zhepos8-lang <zhepos8@gmail.com>
2026-02-16 22:32:57 -0800

    Update category.py

--- ZH_pos/views/category.py
+++ ZH_pos/views/category.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from ZH_pos.models import Category
-from ...woocommerce_api import sync_woo_categories
+from ZH_pos.woocommerce_webhook.py import sync_woo_categories
 
 
 @login_required
