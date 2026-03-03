f47a58eb2a649eccc9a686d344ac3f1d75924136
zhepos9-cloud <zhepos9@gmail.com>
2026-03-03 19:43:44 +0500

    v

--- ZH_pos/views/returns.py
+++ ZH_pos/views/returns.py
@@ -3,7 +3,7 @@ from django.shortcuts import render, get_object_or_404
 from django.http import JsonResponse
 import json
 
-from ZH_pos.models import Order, OrderItem, Return, Sale, SaleReturn, SaleItem, SaleReturnItem
+from ZH_pos.models import Order, OrderItem, Return, Sales, SaleReturn, SaleItem, SaleReturnItem
 
 
 @login_required
