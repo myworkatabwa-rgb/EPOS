4a631303290ef660010ad09d7938f77ad1dac5ca
zhepos9-cloud <zhepos9@gmail.com>
2026-03-04 10:32:47 +0500

    message

--- ZH_pos/views/returns.py
+++ ZH_pos/views/returns.py
@@ -2,6 +2,7 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404
 from django.http import JsonResponse
 import json
+from django.contrib import messages
 
 from ZH_pos.models import Order, OrderItem, Return
 
