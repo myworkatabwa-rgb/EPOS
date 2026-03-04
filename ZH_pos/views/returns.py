12f26d216ef35046d00f70795ba349aa05b2faa9
zhepos9-cloud <zhepos9@gmail.com>
2026-03-04 10:43:43 +0500

    redirect

--- ZH_pos/views/returns.py
+++ ZH_pos/views/returns.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import JsonResponse
 import json
 from django.contrib import messages
