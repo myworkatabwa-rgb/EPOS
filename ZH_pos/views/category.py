d462d20d13199f89889b918b6e0dd69e93d3578c
zhepos9-cloud <zhepos9@gmail.com>
2026-03-04 11:13:28 +0500

    x

--- ZH_pos/views/category.py
+++ ZH_pos/views/category.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from ZH_pos.models import Category
 from ZH_pos.woocommerce_webhook import sync_woo_categories
