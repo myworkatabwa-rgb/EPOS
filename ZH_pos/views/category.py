91e13ce8f772baae3160e44cc0581abe8acd9c0c
zhepos9-cloud <zhepos9@gmail.com>
2026-03-04 11:14:49 +0500

    message import

--- ZH_pos/views/category.py
+++ ZH_pos/views/category.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 from ZH_pos.models import Category
 from ZH_pos.woocommerce_webhook import sync_woo_categories
 import logging
