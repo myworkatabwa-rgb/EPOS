18232687243a77ba95266c1e0accd051ab0a6476
zhepos9-cloud <zhepos9@gmail.com>
2026-03-13 00:28:27 +0500

    d

--- Z_pos/settings.py
+++ Z_pos/settings.py
@@ -21,13 +21,14 @@ DEBUG = True
 ALLOWED_HOSTS = [
     'epos-production-c366.up.railway.app',
     'epos-production-3026.up.railway.app',
-    'epos-production-8d49.up.railway.app'
+    'epos-production-8d49.up.railway.app',
     'epos-production-3b26.up.railway.app'
 ]
 CSRF_TRUSTED_ORIGINS = [
     "https://epos-production-c366.up.railway.app",
     "https://epos-production-3026.up.railway.app",
-    "https://epos-production-8d49.up.railway.app"
+    "https://epos-production-8d49.up.railway.app",
+    "https://epos-production-3b26.up.railway.app"
 ]
 
 # =====================================================
